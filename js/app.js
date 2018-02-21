59dbdce9f83e4626e6236b4ae29bab27eb80d869
Kamila Skonieczna <95kamila.skonieczna@gmial.com>
2018-02-21 12:24:01 +0100

    Fixed media and cleaned sccs

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
+$(".mcBurger").click(function() {
+    $(".topsandwich").toggleClass('topsandwichstart');
+    $(".meat").toggleClass("hidden");
+    $(".downsandwich").toggleClass("downsandwichstart");
+});
+
+
+var menu = document.querySelector('.menu'),
+    menuTrigger = document.querySelector('.mcBurger'),
+    mobile = window.matchMedia('(max-width: 800px)');
+
+function mobileMatches(mobile) {
+    if (mobile.matches) {
+        onMobile();
+    } else {
+        onDesktop();
+    }
+}
+mobileMatches(mobile);
+mobile.addListener( mobileMatches );
+
+menuTrigger.addEventListener( 'click', function(){
+    menu.hidden = !menu.hidden;
+});
+
+function onMobile(){
+    menu.hidden = true;
+    menuTrigger.hidden = false;
+}
+
+function onDesktop(){
+    menu.hidden = false;
+    menuTrigger.hidden = true;
+}
+
+
+
+
 $('a[href*="#"]')
     .not('[href="#"]')
     .not('[href="#0"]')
@@ -29,7 +67,6 @@ $('a[href*="#"]')
         }
     });
 
-
 AOS.init({
-    duration: 1200,
-})
+    duration: 1200
+});
